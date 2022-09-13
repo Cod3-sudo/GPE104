c8b8cace88bfd665cfc902c90d47de62de821575
Cod3-sudo <75404188+Cod3-sudo@users.noreply.github.com>
2022-09-12 17:06:30 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+#Today we will be learning about the IF command in python. The IF command sets a condition
+#and if that condition is met it makes the IF statement true. Depending on whether the IF statement
+#is true or false different outputs will be shown.
+
+#Basic math equation to show the function of an IF statement.
+x = 5*0
+#If statement, proper syntax.
+if (x == 0): 
+    #If x equals zero it will make this IF statement true, causing it to display "X equals Zero"
+    print("X equals zero")
+if (x!=0):
+    #Another IF statement for when x doesn't equal zero.
+    print("No, doesn't equal zero")
